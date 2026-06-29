871bf99be3dff860d1e14a3ece91cc8e189f086f
zhanyingdao2 <zhanyingdao2@gmail.com>
2026-06-29 20:00:10 +0800

    feat: tag favorites-opened videos with fromFav flag

--- lib/pages/fav_detail/controller.dart
+++ lib/pages/fav_detail/controller.dart
@@ -221,18 +221,19 @@ class FavDetailController
       cid: item.ugc!.firstCid!,
       cover: item.cover,
       title: item.title,
-      extraArguments: isPlayAll.value
-          ? {
-              'sourceType': SourceType.fav,
-              'mediaId': folder.id,
-              'oid': item.id,
-              'favTitle': folder.title,
-              'count': folder.mediaCount,
-              'desc': true,
-              if (index != null) 'isContinuePlaying': index != 0,
-              'isOwner': isOwner,
-            }
-          : null,
+      extraArguments: {
+        'fromFav': true,
+        if (isPlayAll.value) ...{
+          'sourceType': SourceType.fav,
+          'mediaId': folder.id,
+          'oid': item.id,
+          'favTitle': folder.title,
+          'count': folder.mediaCount,
+          'desc': true,
+          if (index != null) 'isContinuePlaying': index != 0,
+          'isOwner': isOwner,
+        },
+      },
     );
   }
 }
